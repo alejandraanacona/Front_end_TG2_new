621679390a2564c20cf397e7bee1ec33aaa9f7d3
alejandraanacona <alejandra.anacona@correounivalle.edu.co>
2024-05-06 18:37:03 -0500

    hasta aquí con editor y un directorio no fucnional

--- src/views/ModoProgramado.vue
+++ src/views/ModoProgramado.vue
@@ -1,27 +1,55 @@
 <template>
-  <div class="editor-container">
-    <AceEditor :value="code" />
-  </div>
+  <div class="container">
+    <div class="file-explorer">
+      <FileExplorer :directoryItems="directoryItems" @select="handleFileSelect" />
+    </div>
+    <div class="editor">
+      <AceEditor :value="code" />
+    </div>
+</div>
 </template>
 
 <script>
 import AceEditor from '@/components/Editor/AceEditor.vue';
+import FileExplorer from '@/components/Editor/FileExplorer.vue'; // Supongamos que tienes un componente FileExplorer
 
 
 export default {
   components: {
     AceEditor,
+    FileExplorer,
   },
   data() {
     return {
       code: '<html>\n\t<head>\n\t\t<title>Vue.js Ace Editor</title>\n\t</head>\n\t<body>\n\t\t<h1>Hello, Ace Editor in Vue.js!</h1>\n\t</body>\n</html>',
+      directoryItems: [], // Lista de archivos y directorios para mostrar en FileExplorer
     };
   },
+
+  methods: {
+    handleFileSelect(file) {
+      // Lógica para manejar la selección de un archivo
+    },
+  },
 };
+
 </script>
 
 <style>
-.editor-container {
-  margin-top: 100px; 
+
+
+.container {
+  display: flex;
+  padding-top: 100000px;
+}
+
+.file-explorer {
+  width: 250px; /* Ancho del FileExplorer */
+  padding-top: 51px;
+}
+
+.editor {
+  flex: 1; /* Toma el resto del espacio disponible */
+  padding-top: 100px;
 }
 </style>
